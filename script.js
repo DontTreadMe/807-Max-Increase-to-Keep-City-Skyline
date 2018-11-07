010dbe122998e18e32159d3c29b24d4ee8f66827
ASubjectPro <31659942+DontTreadMe@users.noreply.github.com>
2018-11-07 03:24:39 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxIncreaseKeepingSkyline = function(grid) {
+  let topBottom = [], leftRight = [], gridNew = [];
+  for (let i = 0; i < grid.length; i++) {
+    leftRight.push(Math.max(...grid[i]));
+    for (let j = 0; j < grid[i].length; j++) {
+      if (topBottom[j] === undefined || topBottom[j] < grid[i][j]) {
+        topBottom[j] = grid[i][j];
+      }
+    }
+  }
+  return [leftRight, topBottom];
+  
+};
+
+console.log(maxIncreaseKeepingSkyline([[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]));
